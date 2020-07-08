e1fa31aa2a2aa3d8e3ea3315207258d28a525302
Gustaf Ullman <Gustaf.Ullman@icm.uu.se>
2020-07-08 22:06:44 +0200

    Changes to riboTimesMle.R, Added saveAsExcel.R and saveAsExcel_fragments.R

--- riboTimesMle.R
+++ riboTimesMle.R
@@ -7,12 +7,38 @@ CT<-loadCountsTable(fileName)
 
 Mle <- function(CT){
   
-  Omega_exp = 0
   
-  # Calculate U_exp
+  
+  
   U_exp = 0
   z_k = 0
   g_mod = 0
+  
+  ngenes = length(CT@geneName)
+  
+  codon_context = 15
+  posAsite = 8
+  number_codons = 64
+  
+  Omega_exp = matrix(nrow = number_codons, ncol = codon_context)
+  
+  
+  for(i in 1:ngenes){
+    
+    # Calculate U_exp
+    U_exp[i] = sum(CT@countsPerCodon[[i]])
+    length_gene = length(CT@countsPerCodon[[i]])
+    counts_gene = CT@countsPerCodon[[i]]
+    
+    for(j in posAsite:(length_gene - posAsite)){
+      # Calculate Omega_exp
+      j_relAsite = j - posAsite + 1 # See figure 2 in manuscript
+      counts_j = counts_gene[j]
+      Omega_exp[CT@codonIndex[kA], j_relAsite] = Omega_exp[CT@codonIndex[kA], j_relAsite] + counts_j
+    }
+    
+    
+  }
   
   
   
